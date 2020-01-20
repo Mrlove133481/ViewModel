e77f9067996c7c28c33b576af47a37cc089c29dd
Mrlove133481 <1334819077@qq.com>
2020-01-20 17:21:00 +0800

    使用ViewModel，完善代码完成

--- app/src/main/java/com/mrlove/viewmodel/MainActivity.java
+++ app/src/main/java/com/mrlove/viewmodel/MainActivity.java
@@ -1,14 +1,47 @@
 package com.mrlove.viewmodel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    MyViewModel myViewModel;
+    TextView textView;
+    Button button1;
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        textView = findViewById(R.id.textView2);
+        //官方已经弃用
+        // myViewModel = ViewModelProviders.of(this).get(MyViewModel.class);
+        //获取实例对象
+        myViewModel = new ViewModelProvider(this).get(MyViewModel.class);
+        textView.setText(String.valueOf(myViewModel.num));
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myViewModel.num++;
+                textView.setText(String.valueOf(myViewModel.num));
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myViewModel.num += 2;
+                textView.setText(String.valueOf(myViewModel.num));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/mrlove/viewmodel/MyViewModel.java
@@ -0,0 +1,8 @@
+package com.mrlove.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class MyViewModel extends ViewModel {
+
+    public int num = 0;
+}
